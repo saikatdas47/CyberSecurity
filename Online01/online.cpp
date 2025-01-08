b121991f79af7a97ed0caf7753762afb673eee92
Saikat Das <saikatdasmain47@gmail.com>
2025-01-08 12:04:56 +0600

    3d

---
+++ Online01/online.cpp
@@ -0,0 +1,110 @@
+#include <iostream>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+long long gcd(long long a, long long b)
+{
+    while (b != 0)
+    {
+        long long temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
+long long modExp(long long base, long long exp, long long mod)
+{
+    long long result = 1;
+    base = base % mod;
+    while (exp > 0)
+    {
+        if (exp % 2 == 1)
+        {
+            result = (result * base) % mod;
+        }
+        base = (base * base) % mod;
+        exp = exp / 2;
+    }
+    return result;
+}
+
+long long modInverse(long long a, long long m)
+{
+    long long m0 = m, t, q;
+    long long x0 = 0, x1 = 1;
+    if (m == 1)
+        return 0;
+
+    while (a > 1)
+    {
+
+        q = a / m;
+        t = m;
+        m = a % m;
+        a = t;
+        t = x0;
+
+        x0 = x1 - q * x0;
+        x1 = t;
+    }
+
+    if (x1 < 0)
+        x1 += m0;
+
+    return x1;
+}
+
+int main()
+{
+
+    long long p = 17, q = 11;
+    long long n = p * q;
+    long long phi = (p - 1) * (q - 1);
+
+    long long e;
+    do
+    {
+        e = rand() % phi;
+    } while (gcd(e, phi) != 1);
+    // long long e = 7;
+
+    long long d;
+
+    d = modInverse(e, phi);
+
+    cout << "Public Key: {" << e << ", " << n << "}\n";
+    cout << "Private Key: {" << d << ", " << n << "}\n";
+
+    long long message = 88;
+    // cout << "Original Message: " << message << "\n";
+
+    // long long encryptedMessage = modExp(message, e, n);
+    //      long long encryptedMessage = ((message^ e)% n);
+
+    //  cout << "Encrypted Message: " << encryptedMessage << "\n";
+
+    // long long decryptedMessage = modExp(encryptedMessage, d, n);
+    // cout << "Decrypted Message: " << decryptedMessage << "\n";
+
+    // h=((e)+(d))mod phi(n)
+    int k = 1;
+    long long l = ((e ^ k) + (d));
+    long long h = l % phi;
+
+    cout << "Value of h: " << h << "\n";
+
+    if (gcd(h, phi) == 1)
+    {
+        printf("Valid Hash\n");
+    }
+    else
+    {
+        printf("Invalid\n");
+    }
+
+    return 0;
+}
